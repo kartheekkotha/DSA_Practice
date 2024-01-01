ffdb11602189190d8b0082db3bfa3d1fcef069dc
Kartheek Kotha <kartheekkotha20041@gmail.com>
2024-01-01 18:38:55 +0530

    daily DSa - 1-01-2024

--- LeetcodePractice/1624_LargestSubstringBetweenTwoEqualCharacters.py
+++ LeetcodePractice/1624_LargestSubstringBetweenTwoEqualCharacters.py
@@ -19,3 +19,17 @@ class Solution(object):
                         maxCount = count
                 count+=1
         return maxCount
+    
+
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        first_index = {}
+        ans = -1
+        
+        for i in range(len(s)):
+            if s[i] in first_index:
+                ans = max(ans, i - first_index[s[i]] - 1)
+            else:
+                first_index[s[i]] = i
+
+        return ans
